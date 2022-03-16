a7eb50c927102fc71d889b2c3c85cfb3a3089f49
Ali Arafati <private@macbook-pro-5.lan>
2022-03-16 23:37:37 +0100

    fix responsive

--- docs/_next/static/LoXYuuFhunaBjIxmozazn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1066271239a7e79f.css","static/chunks/pages/index-f9066fe56348dd7f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
