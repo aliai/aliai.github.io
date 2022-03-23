8b9fe1306c75b0df6fc97e161a92307fd5fdde5a
Ali Arafati <private@macbook-pro-5.lan>
2022-03-24 00:19:44 +0100

    24-7

--- docs/_next/static/hUzjwYRSk5mI03XduEHb6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a8574eb5bafe822a.css","static/chunks/pages/index-02d4173443c4688f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ docs/_next/static/kz5JevXZ8SsdsC8uwrsVT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/88a1d60176de7556.css","static/chunks/pages/index-02d4173443c4688f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
